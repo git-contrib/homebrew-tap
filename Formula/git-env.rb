d2c94ad65f47e2b832f86cb7963fb810e8aac5b5
Socheat Sok <alex@socheat.net>
2022-06-29 16:53:19 +0700

    Update git-env formula

--- Formula/git-env.rb
+++ Formula/git-env.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class GitEnv < Formula
   desc "Generate environment variable of the current working directory/repo."
   homepage "https://github.com/git-contrib/git-env"
